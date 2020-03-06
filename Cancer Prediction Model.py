274e647f3ced5a0c7245ba46f9e9d1c41d61dc1e
khaliljammal <61887577+khaliljammal@users.noreply.github.com>
2020-03-06 15:20:04 -0800

    Update Cancer Prediction Model.py

--- Cancer Prediction Model.py
+++ Cancer Prediction Model.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[13]:
-
 
 import pandas as pd
 import numpy as np
@@ -12,8 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 
-
-# In[14]:
 
 
 #A 
@@ -29,14 +22,12 @@ y = df['Malignant_Cancer']
 len(df)
 
 
-# In[15]:
 
 
 #B Use SciKit Learn to split dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 2)
 
 
-# In[16]:
 
 
 #C LOAD MODEL
@@ -44,7 +35,6 @@ my_Decisiontree = DecisionTreeClassifier(random_state=2)
 my_Decisiontree.fit(X_train, y_train)
 
 
-# In[17]:
 
 
 #C GET SCORE
@@ -53,17 +43,11 @@ score_dt = accuracy_score(y_test, y_predict_dt)
 print("DescitionTree" ,score_dt)
 
 
-# In[32]:
-
-
 #C GET FPR, TPR
 fpr, tpr, thresholds = metrics.roc_curve(y_test, y_predict_dt, pos_label=1)
-
 print(fpr)
 print(tpr)
 
-
-# In[33]:
 
 
 # GET AUC SCORE
@@ -71,7 +55,6 @@ AUC = metrics.auc(fpr, tpr)
 print(AUC)
 
 
-# In[34]:
 
 
 #Plot ROC Curve
@@ -101,45 +84,30 @@ plt.legend(loc="lower right")
 plt.show()
 
 
-# In[25]:
-
-
-#D 
 bootstarp_size = 0.8*(len(df))
 for i in range(19):
     df.resample(X_train, n_samples = bootstarp_size , random_state=i , replace = True)
     Base_DecisionTree = DecisionTreeClassifier(random_state=2)
    
 
-
-# In[52]:
-
-
-#E
 from sklearn.ensemble import AdaBoostClassifier
 my_AdaBoost = AdaBoostClassifier(n_estimators = 29,random_state=2)
 
 model = my_AdaBoost.fit(X_train, y_train)
 
 
-# In[56]:
-
 
 y_pred_dt = model.predict(X_test)
 score_dt = accuracy_score(y_test, y_pred_dt)
 print("DescitionTree w/ AdaBoost" ,score_dt)
 
-#C GET FPR, TPR
+# GET FPR, TPR
 fpr_ADA, tpr_ADA, thresholds = metrics.roc_curve(y_test, y_pred_dt, pos_label=1)
 
 AUC = metrics.auc(fpr_ADA, tpr_ADA)
 print("AUC with Ada Boost",AUC)
 
 
-# In[1]:
-
-
-#f
 from xgboost import XGBClassifier
 my_XGBoost = XGBClassifier(n_estimators = 29,random_state=2)
 
@@ -156,10 +124,7 @@ AUC = metrics.auc(fpr_XG, tpr_XG)
 print("AUC with XGBoost",AUC)
 
 
-# In[27]:
 
-
-#G
 from sklearn.ensemble import RandomForestClassifier
 my_RandomForest = RandomForestClassifier(n_estimators = 29, bootstrap = True, random_state=2)
 
@@ -174,16 +139,6 @@ fpr_RF, tpr_RF, thresholds = metrics.roc_curve(y_test, y_pred_dt, pos_label=1)
 
 AUC = metrics.auc(fpr_RF, tpr_RF)
 print("AUC with Random Forest",AUC)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
